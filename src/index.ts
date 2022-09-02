810c1a813ca32af76a4679ffa20b9c50dde7568e
butnothero <butnothero@gmail.com>
2022-09-02 15:26:56 +0700

    feat: added check http error status code

--- src/index.ts
+++ src/index.ts
@@ -55,3 +55,12 @@ export const getIconClassMods = (mods: string | string[], prefix = 'i-'): string
 
   return '';
 };
+
+/**
+ * Является ли код ответа ошибочным
+ * @param code Код ответа
+ */
+export const isErrorHttp = (code: number): boolean => {
+  const _code = Number(code);
+  return _code >= 400 && _code <= 599 && _code !== 0;
+};
