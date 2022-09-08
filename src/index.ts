cf084b4d812c0b59eefaf89890002c35bf028d90
butnothero <butnothero@gmail.com>
2022-09-08 12:43:41 +0700

    ver(1.0.2): added get date range

--- src/index.ts
+++ src/index.ts
@@ -64,3 +64,41 @@ export const isErrorHttp = (code: number): boolean => {
   const _code = Number(code);
   return _code >= 400 && _code <= 599 && _code !== 0;
 };
+
+/**
+ * Получить массив месяцев между двумя датами
+ * @param startDate Дата начала
+ * @param endDate Дата конца
+ */
+export const getDateRange = (startDate, endDate) => {
+  const start = startDate.split('-');
+  const end = endDate.split('-');
+  const startYear = parseInt(start[0]);
+  const endYear = parseInt(end[0]);
+  const dates: string[] = [];
+
+  for (let i = startYear; i <= endYear; i++) {
+    const endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
+    const startMon = i === startYear ? parseInt(start[1]) - 1 : 0;
+    for (let j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j + 1) {
+      const month = j + 1;
+      const displayMonth = month < 10 ? '0' + month : month;
+      dates.push([i, displayMonth, '01'].join('-'));
+    }
+  }
+  return dates;
+};
+
+/**
+ * Получить массив дней между двумя датами
+ * @param startDate Дата начала
+ * @param endDate Дата конца
+ */
+export const getDateDaysRange = (startDate, endDate) => {
+  const arr: Date[] = [];
+  const dt = new Date(startDate);
+  for (; dt <= new Date(endDate); dt.setDate(dt.getDate() + 1)) {
+    arr.push(new Date(dt));
+  }
+  return arr;
+};
