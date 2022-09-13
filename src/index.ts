05675b3cab0b572818d565243ab9e5058bc89823
butnothero <butnothero@gmail.com>
2022-09-13 17:36:13 +0700

    ver(1.0.5): added getBool

--- src/index.ts
+++ src/index.ts
@@ -128,3 +128,14 @@ export const getDateDaysRange = (startDate, endDate) => {
   }
   return arr;
 };
+
+/**
+ * Получить значение в булевом формате
+ * @param value Значение
+ */
+export const getBool = (value: unknown): boolean => {
+  if (typeof value === 'boolean') return value;
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+  return false;
+};
